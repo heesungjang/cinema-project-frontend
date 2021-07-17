2914ad1fa26c0d11bf6c66181125c1b6437fb444
choice91 <wer1264@gmail.com>
2021-07-17 20:33:29 +0900

    MainMovieList에 평점(star) 수정
    평점 숫자 추가

--- src/Components/MainMovieList.js
+++ src/Components/MainMovieList.js
@@ -33,42 +33,49 @@ const MainMovieList = (props) => {
       url: "https://caching.lottecinema.co.kr//Media/MovieFile/MovieImg/202004/15496_101_1.jpg",
       rank: "1",
       rate: "39.4%",
+      star: "9",
     },
     {
       name: "랑종",
       url: "https://caching.lottecinema.co.kr//Media/MovieFile/MovieImg/202107/17630_101_1.jpg",
       rank: "2",
       rate: "31.1%",
+      star: "6",
     },
     {
       name: "보스 베이비2",
       url: "https://caching.lottecinema.co.kr//Media/MovieFile/MovieImg/202107/17623_101_1.jpg",
       rank: "3",
       rate: "9.9%",
+      star: "0",
     },
     {
       name: "이스케이프 룸 2: 노 웨이 아웃",
       url: "https://caching.lottecinema.co.kr//Media/MovieFile/MovieImg/202107/17598_101_1.jpg",
       rank: "4",
       rate: "6.0%",
+      star: "8.2",
     },
     {
       name: "",
       url: "https://caching2.lottecinema.co.kr/lotte_image/2021/Ido/Ido_184262.jpg",
       rank: "AD",
       rate: "",
+      star: "",
     },
     {
       name: "크루엘라",
       url: "https://caching.lottecinema.co.kr//Media/MovieFile/MovieImg/202105/17387_101_1.jpg",
       rank: "5",
       rate: "3.0%",
+      star: "9.2",
     },
     {
       name: "꽃다발 같은 사랑을 했다.",
       url: "https://caching.lottecinema.co.kr//Media/MovieFile/MovieImg/202106/17526_101_1.jpg",
       rank: "6",
       rate: "1.0%",
+      star: "8.6",
     },
   ];
 
@@ -151,8 +158,9 @@ function Item(props) {
                       margin: "auto 10px",
                     }}
                   />
-                  <span>
+                  <span style={{ display: "flex", alignItems: "center" }}>
                     <img src={star} alt="" />
+                    <span style={{ marginLeft: "3px" }}>{props.item.star}</span>
                   </span>
                   <Divider
                     orientation="vertical"
