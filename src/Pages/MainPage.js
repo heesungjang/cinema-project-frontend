1c150ee399dd079e7e3a929fd8e492f9f5f9377c
choice91 <wer1264@gmail.com>
2021-07-17 18:19:44 +0900

    EventComponent 추가
    메인화면에 이벤트 컴포넌트 추가. 광고이미지 배치

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -7,28 +7,30 @@ import MainHeaderLayout from "../Layouts/Main/MainHeaderLayout";
 import MainPageLayout from "../Layouts/Main/MainPageLayout";
 import MainSubSectionLayout from "../Layouts/Main/MainSubSectionLayout";
 import MainSubSection from "../Components/MainSubSection";
+import EventComponent from "../Components/EventComponent";
+
 const MainPage = (props) => {
-    return (
-        <React.Fragment>
-            <MainPageLayout>
-                <MainHeaderLayout>
-                    <Header page={"main"} />
-                </MainHeaderLayout>
-                <MainSubSectionLayout>
-                    <MainSubSection />
-                </MainSubSectionLayout>
-                <MainContentLayout>
-                    <MainMovieList />
-                    <div style={{ height: "400px", backgroundColor: "blue" }}>
-                        여기 이벤트
-                    </div>
-                    <div style={{ height: "150px", backgroundColor: "yellow" }}>
-                        시사회/무대인사
-                    </div>
-                </MainContentLayout>
-            </MainPageLayout>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <MainPageLayout>
+        <MainHeaderLayout>
+          <Header page={'main'} />
+        </MainHeaderLayout>
+        <MainSubSectionLayout>
+          <MainSubSection />
+        </MainSubSectionLayout>
+        <MainContentLayout>
+          <MainMovieList />
+          <div style={{ height: "641px", backgroundColor: "wheat" }}>
+            <EventComponent />
+          </div>
+          <div style={{ height: "150px", backgroundColor: "yellow" }}>
+            시사회/무대인사
+          </div>
+        </MainContentLayout>
+      </MainPageLayout>
+    </React.Fragment>
+  );
 };
 
 export default MainPage;
