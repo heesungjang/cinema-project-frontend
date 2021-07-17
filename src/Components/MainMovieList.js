17700f433659b87c178e363280661de81938720a
choice91 <wer1264@gmail.com>
2021-07-17 14:19:19 +0900

    MainMovieList 수정
    react slick 사용. 영화 포스터를 5개씩 보여주고 좌우버튼 눌렀을 때 하나씩 이동

--- src/Components/MainMovieList.js
+++ src/Components/MainMovieList.js
@@ -1,61 +1,78 @@
 import React from "react";
 import Slider from "react-slick";
+import styled from "styled-components";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { makeStyles } from "@material-ui/core/styles";
 import { Card, CardMedia } from "@material-ui/core";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles({
   root: {
-    flexGrow: 1,
     maxWidth: 185,
     maxHeight: 262,
   },
-  paper: {
+  media: {
     height: 262,
-    width: 185,
   },
-  control: {
-    padding: theme.spacing(2),
-  },
-}));
+});
 
 const MainMovieList = (props) => {
+  const classes = useStyles();
   const items = [
     {
       name: "Random Name #1",
       url: "https://caching.lottecinema.co.kr//Media/MovieFile/MovieImg/202004/15496_101_1.jpg",
+      rank: "1",
     },
     {
       name: "Random Name #2",
       url: "https://caching.lottecinema.co.kr//Media/MovieFile/MovieImg/202107/17630_101_1.jpg",
+      rank: "2",
+    },
+    {
+      name: "Random Name #3",
+      url: "https://caching.lottecinema.co.kr//Media/MovieFile/MovieImg/202107/17623_101_1.jpg",
+      rank: "3",
+    },
+    {
+      name: "Random Name #4",
+      url: "https://caching.lottecinema.co.kr//Media/MovieFile/MovieImg/202107/17598_101_1.jpg",
+      rank: "4",
+    },
+    {
+      name: "Random Name #5",
+      url: "https://caching2.lottecinema.co.kr/lotte_image/2021/Ido/Ido_184262.jpg",
+      rank: "AD",
+    },
+    {
+      name: "Random Name #6",
+      url: "https://caching.lottecinema.co.kr//Media/MovieFile/MovieImg/202105/17387_101_1.jpg",
+      rank: "5",
+    },
+    {
+      name: "Random Name #7",
+      url: "https://caching.lottecinema.co.kr//Media/MovieFile/MovieImg/202106/17526_101_1.jpg",
+      rank: "6",
     },
   ];
 
   const settings = {
-    dots: true,
-    infinite: true,
+    dots: false,
+    infinite: false,
     speed: 500,
-    slidesToShow: 1,
+    slidesToShow: 5,
     slidesToScroll: 1,
   };
 
   return (
     <>
-      <Slider {...settings}>
-        {items.map((item, i) => (
-          <Item key={i} item={item} />
-        ))}
-      </Slider>
-      {/* <span>card</span>
-            <span>card</span>
-            <span>card</span>
-            <span>card</span>
-            <span>card</span>
-            <span>card</span>
-            <span>card</span>
-            <span>card</span>
-            <span>card</span> */}
+      <Container>
+        <Slider {...settings}>
+          {items.map((item, i) => (
+            <Item key={i} item={item} />
+          ))}
+        </Slider>
+      </Container>
     </>
   );
 };
@@ -67,9 +84,27 @@ function Item(props) {
     <>
       <Card className={classes.root}>
         <CardMedia className={classes.media} image={props.item.url} />
+        <span
+          style={{
+            position: "absolute",
+            bottom: "0px",
+            color: "white",
+            fontWeight: "bold",
+            fontSize: "26px",
+            fontStyle: "italic",
+            padding: "0px 0px 0px 10px",
+          }}
+        >
+          {props.item.rank}
+        </span>
       </Card>
     </>
   );
 }
+
+const Container = styled.div`
+  width: 980px;
+  margin: 0 auto;
+`;
 
 export default MainMovieList;
--- src/Layouts/Main/MainContentLayout.js
+++ src/Layouts/Main/MainContentLayout.js
@@ -2,20 +2,21 @@ import React from "react";
 import { Grid } from "@material-ui/core";
 
 const MainContentLayout = (props) => {
-    return (
-        <Grid
-            xs={12}
-            style={{
-                width: "75%",
-                backgroundColor: "yellow",
-                display: "flex",
-                margin: "auto",
-                flexDirection: "column",
-            }}
-        >
-            {props.children}
-        </Grid>
-    );
+  return (
+    <Grid
+      xs={12}
+      style={{
+        // width: "75%",
+        width: "100%",
+        backgroundColor: "black",
+        display: "flex",
+        margin: "auto",
+        flexDirection: "column",
+      }}
+    >
+      {props.children}
+    </Grid>
+  );
 };
 
 export default MainContentLayout;
