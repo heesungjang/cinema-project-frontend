6f5b88130140608c0c9743e9bcd9177aec8c4c4e
choice91 <wer1264@gmail.com>
2021-07-17 15:41:16 +0900

    Header 스타일 수정
    아이콘, 폰트사이즈 수정

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,72 +3,80 @@ import { Grid, Typography, Container, Divider } from "@material-ui/core";
 import HeaderLinks from "./HeaderLinks";
 
 import logo from "../images/logo.png";
+import facebook from "../images/gnb_facebook_wht.png";
+import youtube from "../images/gnb_youtube_wht.png";
+import follow from "../images/gnb_follow_wht.png";
 import logo_red from "../images/logo_red.png";
 
 const Header = (props) => {
-    const { page } = props;
-    return (
-        <Container style={{ height: "115px", alignItems: "center" }}>
-            <Grid xs={12} style={{ display: "flex", marginTop: "30px" }}>
-                <Grid
-                    xs={4}
-                    style={{
-                        display: "flex",
-                        color: page === "main" ? "#fff" : "#000",
-                    }}
-                >
-                    <Typography>좋아요</Typography>
-                    <Typography style={{ margin: "0 15px" }}>
-                        구독하기
-                    </Typography>
-                    <Typography>팔로우</Typography>
-                </Grid>
+	const { page } = props
+  return (
+    <Container style={{ height: "115px", alignItems: "center" }}>
+      <Grid xs={12} style={{ display: "flex", marginTop: "30px" }}>
+        <Grid
+          xs={4}
+          style={{ display: "flex", color: page === 'main' ? "#fff" : '#000', alignItems: "center" }}
+        >
+          <Typography>
+            <span>
+              <img src={facebook} alt="" />
+            </span>
+            <span style={{ margin: "0px 0px 0px 5px", fontSize: "12px" }}>
+              좋아요
+            </span>
+          </Typography>
+          <Typography style={{ margin: "0 15px" }}>
+            <span>
+              <img src={youtube} alt="" />
+            </span>
+            <span style={{ margin: "0px 0px 0px 5px", fontSize: "12px" }}>
+              구독하기
+            </span>
+          </Typography>
+          <Typography>
+            <span>
+              <img src={follow} alt="" />
+            </span>
+            <span style={{ margin: "0px 0px 0px 5px", fontSize: "12px" }}>
+              팔로우
+            </span>
+          </Typography>
+        </Grid>
 
-                <Grid
-                    xs={4}
-                    style={{
-                        display: "flex",
-                        justifyContent: "center",
-                    }}
-                >
-                    <Typography>
-                        <img
-                            src={page === "main" ? logo : logo_red}
-                            alt="Lotte Cinema"
-                        />
-                    </Typography>
-                </Grid>
+        <Grid xs={4} style={{ display: "flex", justifyContent: "center" }}>
+          <Typography>
+            <img src={logo} alt="Lotte Cinema" />
+          </Typography>
+        </Grid>
 
-                <Grid
-                    xs={4}
-                    style={{
-                        display: "flex",
-                        justifyContent: "flex-end",
-                        color: page === "main" ? "#fff" : "#000",
-                        fontSize: "12px",
-                    }}
-                >
-                    <Typography>멤버십</Typography>
-                    <Typography style={{ margin: "0 15px" }}>
-                        고객센터
-                    </Typography>
-                    <Typography>로그인</Typography>
-                </Grid>
-            </Grid>
+        <Grid
+          xs={4}
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            color: "#fff",
+            fontSize: "12px",
+            alignItems: "center",
+          }}
+        >
+          <Typography style={{ fontSize: "12px" }}>멤버십</Typography>
+          <Typography style={{ margin: "0 15px", fontSize: "12px" }}>
+            고객센터
+          </Typography>
+          <Typography style={{ fontSize: "12px" }}>로그인</Typography>
+        </Grid>
+      </Grid>
 
-            <Divider
-                variant="middle"
-                style={{
-                    margin: "10px 0",
-                    backgroundColor: page === "main" ? "#fff" : "#000",
-                }}
-            />
+      <Divider
+        variant="middle"
+        style={{ margin: "10px 0", backgroundColor: "#fff" }}
+      />
 
-            <Grid xs={12} style={{ display: "flex", justifyContent: "center" }}>
-                <HeaderLinks page={page} />
-            </Grid>
-        </Container>
-    );
+      <Grid xs={12} style={{ display: "flex", justifyContent: "center" }}>
+        <HeaderLinks />
+      </Grid>
+    </Container>
+  );
 };
 
 export default Header;
