5982a0c7a49a6d6b6e20a3027fbece7b2c5cdd30
choice91 <wer1264@gmail.com>
2021-07-17 12:12:50 +0900

    update main

--- src/Components/MainMovieList.js
+++ src/Components/MainMovieList.js
@@ -1,8 +1,53 @@
 import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { makeStyles } from "@material-ui/core/styles";
+import { Card, CardMedia } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    maxWidth: 185,
+    maxHeight: 262,
+  },
+  paper: {
+    height: 262,
+    width: 185,
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
 
 const MainMovieList = (props) => {
-    return (
-        <>
+  const items = [
+    {
+      name: "Random Name #1",
+      url: "https://caching.lottecinema.co.kr//Media/MovieFile/MovieImg/202004/15496_101_1.jpg",
+    },
+    {
+      name: "Random Name #2",
+      url: "https://caching.lottecinema.co.kr//Media/MovieFile/MovieImg/202107/17630_101_1.jpg",
+    },
+  ];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <>
+      <Slider {...settings}>
+        {items.map((item, i) => (
+          <Item key={i} item={item} />
+        ))}
+      </Slider>
+      {/* <span>card</span>
             <span>card</span>
             <span>card</span>
             <span>card</span>
@@ -10,10 +55,21 @@ const MainMovieList = (props) => {
             <span>card</span>
             <span>card</span>
             <span>card</span>
-            <span>card</span>
-            <span>card</span>
-        </>
-    );
+            <span>card</span> */}
+    </>
+  );
 };
+
+function Item(props) {
+  const classes = useStyles();
+
+  return (
+    <>
+      <Card className={classes.root}>
+        <CardMedia className={classes.media} image={props.item.url} />
+      </Card>
+    </>
+  );
+}
 
 export default MainMovieList;
