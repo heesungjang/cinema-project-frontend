d225a5c2062d2a563f24c29811ac27415796b2c2
Heesung jang <heesungj7@gmail.com>
2021-07-17 12:31:42 +0900

    detail info 버튼 스타일 추가
    useStyle 훅 사용, 하나의 스타일 클래스로 버튼 스타일 적용.

--- src/Components/DetailInfo.js
+++ src/Components/DetailInfo.js
@@ -1,17 +1,29 @@
 import React from "react";
 
+import { makeStyles } from "@material-ui/styles";
 import { Grid, Button } from "@material-ui/core";
 
+const useStyles = makeStyles({
+    button: {
+        padding: "15px",
+        borderTop: "None",
+        borderLeft: "None",
+        borderRight: "None",
+    },
+});
+
 const DetailInfo = (props) => {
+    const classes = useStyles();
+
     return (
         <Grid xs={12} style={{ display: "flex", margin: "35px 0" }}>
             <Grid xs={6}>
-                <Button variant="outlined" fullWidth>
+                <Button className={classes.button} variant="outlined" fullWidth>
                     영화정보
                 </Button>
             </Grid>
             <Grid xs={6}>
-                <Button variant="outlined" fullWidth>
+                <Button className={classes.button} variant="outlined" fullWidth>
                     평점 및 관람편 (98)
                 </Button>
             </Grid>
