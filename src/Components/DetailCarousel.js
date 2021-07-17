3ec260e5978e387e95b9e23645d6b3b1d9f77cc5
choice91 <wer1264@gmail.com>
2021-07-17 21:13:21 +0900

    DetailCarousel 수정
    Carousel 이미지 상,하단에 그라데이션효과 추가

--- src/Components/DetailCarousel.js
+++ src/Components/DetailCarousel.js
@@ -1,6 +1,8 @@
 import React from "react";
+import styled from "styled-components";
 import Carousel from "react-material-ui-carousel";
 import { Paper, Button } from "@material-ui/core";
+import { Gradient } from "@material-ui/icons";
 
 const DetailCarousel = (props) => {
   var items = [
@@ -23,30 +25,50 @@ const DetailCarousel = (props) => {
   ];
 
   return (
-    <Carousel animation="slide" navButtonsAlwaysVisible="true">
-      {items.map((item, i) => (
-        <Item key={i} item={item} />
-      ))}
-    </Carousel>
+    <>
+      <Carousel animation="slide" navButtonsAlwaysVisible="true">
+        {items.map((item, i) => (
+          <Item key={i} item={item} />
+        ))}
+      </Carousel>
+      <Gradation location="top" />
+      <Gradation top="624" />
+    </>
   );
 };
 
 const Item = (props) => {
   return (
-    <Paper
-      style={{
-        borderRadius: "0px",
-        justifyContent: "Center",
-        display: "flex",
-        // height: "450px",
-        height: "774px",
-        backgroundColor: "#000000",
-      }}
-    >
-      <img src={props.item.img} alt="" />
-      {/* <Button className="CheckButton">Check it out!</Button> */}
-    </Paper>
+    <>
+      <Paper
+        style={{
+          borderRadius: "0px",
+          justifyContent: "Center",
+          display: "flex",
+          // height: "450px",
+          height: "774px",
+          backgroundColor: "#000000",
+          background:
+            "linear-gradient(to-bottom, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 1) 85%, rgba(0, 0, 0, 1) 100%)",
+        }}
+      >
+        <img src={props.item.img} alt="" />
+        {/* <Button className="CheckButton">Check it out!</Button> */}
+      </Paper>
+    </>
   );
 };
+
+const Gradation = styled.div`
+  width: 100%;
+  height: 150px;
+  position: absolute;
+  ${(props) => (props.top ? `top: ${props.top}px;` : "top: 0px;")}
+  left: 0;
+  ${(props) =>
+    props.location === "top"
+      ? "background: linear-gradient(to top, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 1) 90%, rgba(0, 0, 0, 1) 100%);"
+      : "background: linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 1) 90%, rgba(0, 0, 0, 1) 100%);"}
+`;
 
 export default DetailCarousel;
