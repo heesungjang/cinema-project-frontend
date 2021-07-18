8f93970287f7eb4cb6652db9dab7eeb0c8f4f199
Heesung jang <heesungj7@gmail.com>
2021-07-18 20:27:07 +0900

    로그인 페이지 컨테이너 프리젠터 디자인 패턴 적용
    로그인 페이지의 사이즈가 너무 커져서 view를 담당하는 presenter와 logic을 담당하는 container로 나누었다.

---
+++ src/Components/LoginPresenter.js
@@ -0,0 +1,156 @@
+import React from "react";
+
+import { makeStyles } from "@material-ui/styles";
+import { Button, Grid, TextField, Typography } from "@material-ui/core";
+import DetailContentLayout from "../Layouts/Detail/DetailContentLayout";
+
+const useStyles = makeStyles({
+    mainContainer: {
+        display: "flex",
+        marginTop: "120px",
+        justifyContent: "center",
+        marginBottom: "30px",
+    },
+    activeSubContainer: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        paddingBottom: "5px",
+        borderBottom: "2px solid",
+    },
+    inactiveSubContainer: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        paddingBottom: "5px",
+        borderBottom: "1px solid grey",
+        color: "grey",
+    },
+    formMainContainer: {
+        backgroundColor: "#F5F5F5",
+    },
+    formSubContainer: {
+        padding: "60px 20px",
+    },
+    formContents: {
+        display: "flex",
+        alignItems: "center",
+    },
+    inputContainer: {
+        display: "flex",
+        flexDirection: "column",
+    },
+    loginButton: {
+        padding: "10px",
+        backgroundColor: "#FF253E",
+        fontWeight: "500",
+        fontSize: "15px",
+        height: "112px",
+        width: "112px",
+        marginLeft: "10px",
+    },
+    outerInputContainer: {
+        display: "flex",
+        marginTop: "10px",
+    },
+
+    signupButtonContainer: {
+        marginTop: "20px",
+        display: "flex",
+        justifyContent: "flex-end",
+    },
+    signupText: {
+        color: "grey",
+    },
+});
+
+const LoginPresenter = (props) => {
+    const classes = useStyles();
+    return (
+        <DetailContentLayout>
+            <Grid xs={12} className={classes.mainContainer}>
+                <Grid className={classes.activeSubContainer} xs={4}>
+                    회원
+                </Grid>
+                <Grid className={classes.inactiveSubContainer} xs={4}>
+                    비회원
+                </Grid>
+                <Grid className={classes.inactiveSubContainer} xs={4}>
+                    소셜 로그인
+                </Grid>
+            </Grid>
+            <Grid className={classes.formMainContainer} xs={12}>
+                <Grid className={classes.formSubContainer}>
+                    <Grid xs={12} className={classes.formContents}>
+                        <Grid xs={6}>
+                            <Typography>
+                                L.POINT, 롯데시네마 ID로 별도의 회원가입 없이
+                                이용 가능 합니다.
+                            </Typography>
+                            <Grid className={classes.outerInputContainer}>
+                                <Grid
+                                    xs={12}
+                                    className={classes.inputContainer}
+                                >
+                                    <TextField
+                                        id="outlined-password-input"
+                                        label="아이디 또는 이메일을 입력해 주세요."
+                                        type="text"
+                                        autoComplete="current-password"
+                                        variant="outlined"
+                                    />
+                                    <TextField
+                                        id="outlined-password-input"
+                                        label="비밀번호를 입력해 주세요."
+                                        type="password"
+                                        autoComplete="current-password"
+                                        variant="outlined"
+                                    />
+                                </Grid>
+                                <Button
+                                    className={classes.loginButton}
+                                    variant="contained"
+                                    color="secondary"
+                                >
+                                    로그인
+                                </Button>
+                            </Grid>
+                            <Grid
+                                xs={12}
+                                className={classes.signupButtonContainer}
+                            >
+                                <Grid>
+                                    <Button className={classes.signupText}>
+                                        회원가입
+                                    </Button>
+                                    <Button className={classes.signupText}>
+                                        아이디 찾기
+                                    </Button>
+                                    <Button className={classes.signupText}>
+                                        비밀번호 찾기
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid
+                            xs={6}
+                            style={{
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                            }}
+                        >
+                            <img
+                                style={{ width: "400px" }}
+                                src="https://caching2.lottecinema.co.kr/lotte_image/2021/Inbyu/0317/Inbyu_450220.png"
+                                alt=""
+                            />
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </DetailContentLayout>
+    );
+};
+
+export default LoginPresenter;
--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,10 +1,13 @@
 import React from "react";
+
 import Footer from "../Components/Footer";
 import Header from "../Components/Header";
 import DetailAd from "../Elements/DetailAd";
 import DetailLastAd from "../Elements/DetailLastAd";
-import DetailHeaderLayout from "../Layouts/Detail/DetailHeaderLayout";
+import LoginContainer from "../Components/LoginContainer";
+
 import MainPageLayout from "../Layouts/Main/MainPageLayout";
+import DetailHeaderLayout from "../Layouts/Detail/DetailHeaderLayout";
 
 const LoginPage = () => {
     return (
@@ -12,6 +15,7 @@ const LoginPage = () => {
             <DetailHeaderLayout>
                 <Header page={"detail"} />
             </DetailHeaderLayout>
+            <LoginContainer />
             <DetailAd />
             <DetailLastAd />
             <DetailHeaderLayout>
