80f2f6653b1b1985566ff3a6420a58366b50ef72
Heesung jang <heesungj7@gmail.com>
2021-07-18 18:47:20 +0900

    detail content 스타일 코드 refactor

--- src/Components/DetailContent.js
+++ src/Components/DetailContent.js
@@ -1,120 +1,160 @@
 import React from "react";
 
-import styled from "styled-components";
+import star from "../images/star_14.png";
+import { makeStyles } from "@material-ui/styles";
 import ShareIcon from "@material-ui/icons/Share";
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 import { Grid, Typography, Avatar, Divider, Button } from "@material-ui/core";
 
-const detailInfoContainer = styled.div`
-    display: "flex";
-`;
+const useStyles = makeStyles({
+    postCard: {
+        height: "290px",
+        width: "200px",
+        position: "absolute",
+        top: "-40px",
+        left: "120px",
+        borderRadius: "5px",
+    },
+    mainContainer: {
+        display: "flex",
+    },
+    subContainer: {
+        display: "flex",
+        position: "relative",
+    },
+    detailContainer: {
+        margin: "40px 0px 0 150px",
+    },
+    titleContainer: {
+        display: "flex",
+        marginBottom: "20px",
+    },
+    rateContainer: {
+        display: "flex",
+        justifyContent: "space-between",
+    },
+    rateSubContainer: {
+        display: "flex",
+        alignItems: "center",
+    },
+    subContainerText: {
+        marginLeft: "5px",
+        fontWeight: "700",
+    },
+    detailInfoContainer: {
+        display: "flex",
+    },
+    detailInfoContentContainer: {
+        display: "flex",
+        marginLeft: "10px",
+    },
+    detailInfoContentNameContainer: {
+        display: "flex",
+        marginLeft: "10px",
+        justifyContent: "space-between",
+    },
+    buttonContainer: {
+        marginTop: "20px",
+        display: "flex",
+        justifyContent: "flex-end",
+        alignItems: "center",
+    },
+    button: {
+        borderRadius: "25px",
+        padding: "10px 70px",
+    },
+    startImage: {
+        height: "15px",
+        margin: "0 2px 5px 10px",
+    },
+    divider: {
+        margin: "20px 0",
+    },
+});
 
 const DetailContent = () => {
+    const classes = useStyles();
     return (
-        <Grid xs={12} style={{ display: "flex" }}>
-            <Grid xs={3} style={{ display: "flex", position: "relative" }}>
+        <Grid xs={12} className={classes.mainContainer}>
+            <Grid xs={3} className={classes.subContainer}>
                 <Grid>
                     <img
-                        style={{
-                            height: "290px",
-                            width: "200px",
-                            position: "absolute",
-                            top: "-40px",
-                            left: "120px",
-                            borderRadius: "5px",
-                        }}
+                        className={classes.postCard}
                         src="https://caching.lottecinema.co.kr//Media/MovieFile/MovieImg/202106/17508_103_1.jpg"
                         alt=""
                     />
                 </Grid>
             </Grid>
-            <Grid xs={9} style={{ margin: "40px 0px 0 150px" }}>
-                <Grid
-                    xs={12}
-                    style={{ display: "flex", flexDirection: "column" }}
-                >
-                    <Grid style={{ display: "flex", marginBottom: "20px" }}>
+            <Grid xs={9} className={classes.detailContainer}>
+                <Grid>
+                    <Grid className={classes.titleContainer}>
                         <Avatar style={{ marginRight: "10px" }}>12</Avatar>
                         <Typography variant="h4">빛나는 순간</Typography>
                     </Grid>
-                    <Grid
-                        xs={7}
-                        style={{
-                            display: "flex",
-                            justifyContent: "space-between",
-                        }}
-                    >
-                        <detailInfoContainer>
+                    <Grid xs={7} className={classes.rateContainer}>
+                        <Grid className={classes.rateSubContainer}>
                             <Typography>관람객 평점</Typography>
-                            <Typography>8.9</Typography>
-                        </detailInfoContainer>
-                        <detailInfoContainer>
+                            <img
+                                src={star}
+                                alt=""
+                                className={classes.startImage}
+                            />
+                            <Typography style={{ fontWeight: "700" }}>
+                                8.9
+                            </Typography>
+                        </Grid>
+                        <Grid className={classes.rateSubContainer}>
                             <Typography>예매율</Typography>
-                            <Typography>17위 0.0%</Typography>
-                        </detailInfoContainer>
-                        <detailInfoContainer>
+                            <Typography className={classes.subContainerText}>
+                                17위 0.0%
+                            </Typography>
+                        </Grid>
+                        <Grid className={classes.rateSubContainer}>
                             <Typography>누적관객수</Typography>
-                            <Typography>14,728 명</Typography>
-                        </detailInfoContainer>
+                            <Typography className={classes.subContainerText}>
+                                14,728 명
+                            </Typography>
+                        </Grid>
                     </Grid>
                 </Grid>
-                <Divider style={{ margin: "20px 0" }} />
+                <Divider className={classes.divider} />
                 <Grid xs={12}>
-                    <Grid
-                        xs={5}
-                        style={{
-                            display: "flex",
-                            justifyContent: "space-between",
-                        }}
-                    >
+                    <Grid xs={5} className={classes.detailInfoContainer}>
                         <Typography>장르</Typography>
-                        <Typography>드라마 / 한국</Typography>
-                        <Divider orientation="vertical" />
-                        <Typography>2021.06.30 개봉</Typography>
-                        <Divider orientation="vertical" />
-                        <Typography>95분</Typography>
+                        <Grid className={classes.detailInfoContentContainer}>
+                            <Typography>드라마 / 한국</Typography>
+                            <Typography>2021.06.30 개봉</Typography>
+                            <Typography>95분</Typography>
+                        </Grid>
                     </Grid>
-                    <Grid
-                        xs={2}
-                        style={{
-                            display: "flex",
-                            justifyContent: "space-between",
-                        }}
-                    >
+                    <Grid xs={2} className={classes.detailInfoContainer}>
                         <Typography>감독</Typography>
-                        <Typography>소준문</Typography>
+                        <Grid className={classes.detailInfoContentContainer}>
+                            <Typography>소준문</Typography>
+                        </Grid>
                     </Grid>
-                    <Grid
-                        xs={5}
-                        style={{
-                            display: "flex",
-                            justifyContent: "space-between",
-                        }}
-                    >
+                    <Grid className={classes.detailInfoContainer}>
                         <Typography>출연</Typography>
-                        <Typography>고두심</Typography>
-                        <Typography>지형우</Typography>
-                        <Typography>양정원</Typography>
-                        <Typography>전혜진</Typography>
+                        <Grid
+                            xs={4}
+                            className={classes.detailInfoContentNameContainer}
+                        >
+                            <Typography>고두심</Typography>
+                            <Typography>지형우</Typography>
+                            <Typography>양정원</Typography>
+                            <Typography>전혜진</Typography>
+                        </Grid>
                     </Grid>
                 </Grid>
-                <Grid
-                    xs={12}
-                    style={{
-                        marginTop: "20px",
-                        display: "flex",
-                        justifyContent: "flex-end",
-                        alignItems: "center",
-                    }}
-                >
+                <Grid xs={12} className={classes.buttonContainer}>
                     <ShareIcon />
                     <FavoriteBorderIcon style={{ margin: "0px 40px" }} />
                     <Button
                         variant="contained"
                         color="secondary"
-                        style={{ borderRadius: "25px", padding: "10px 30px" }}
+                        className={classes.button}
                     >
-                        Secondary
+                        예매하기
                     </Button>
                 </Grid>
             </Grid>
