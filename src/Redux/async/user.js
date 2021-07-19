94ce887d39183abf4578563ff5859eebd89802c6
Heesung jang <heesungj7@gmail.com>
2021-07-19 19:28:17 +0900

    signup 비동기 handler 작업 시작

--- src/Redux/async/user.js
+++ src/Redux/async/user.js
@@ -1,16 +1,25 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-
-const delay = (time, value) =>
-    new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(value);
-        }, time);
-    });
+import { authApi } from "../../api";
 
 export const logIn = createAsyncThunk("user/logIn", async (data, thunkAPI) => {
-    const result = await delay(500, {
-        email: data.email,
-        password: data.password,
-    });
-    return result;
+    const result = await authApi.userLogin(data.email, data.password);
+    console.log(result);
+    // const result = await delay(500, {
+    //     email: data.email,
+    //     password: data.password,
+    // });
+    // return result;
 });
+
+export const signup = createAsyncThunk(
+    "user/signup",
+    async (data, thunkAPI) => {
+        const result = await authApi.userSignup(
+            data.name,
+            data.email,
+            data.password,
+            data.phone,
+            data.birthday
+        );
+    }
+);
