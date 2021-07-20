60758a10f0faaa1006c58d76429c49403395a7cb
Heesung jang <heesungj7@gmail.com>
2021-07-20 10:00:44 +0900

    axios 배포 서버  endpoint 적용

--- src/api.js
+++ src/api.js
@@ -1,18 +1,18 @@
 import axios from "axios";
 
-const api = axios.create({
-    baseURL: "http://localhost:4000",
+export const api = axios.create({
+    baseURL: "http://13.209.84.245/",
     // header: {
     //     Authorization: "bearer accessKey",
     // },
 });
 
-export const authApi = {
-    userLogin: () => api.post("/login"),
-    userSignup: () => api.post("/signup"),
-};
+// export const authApi = {
+//     userLogin: () => api.post("/login"),
+//     userSignup: (data) => api.post("/users", { ...data }),
+// };
 
-export const moviesApi = {
-    getAllMovies: () => api.get("/movies"),
-    getMovie: (id) => api.get("/movies/id"),
-};
+// export const moviesApi = {
+//     getAllMovies: () => api.get("/movies"),
+//     getMovie: (id) => api.get("/movies/id"),
+// };
