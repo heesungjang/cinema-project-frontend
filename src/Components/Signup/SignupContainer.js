c7ed7a413c3869edb12ab631354db17a06f36445
Heesung jang <heesungj7@gmail.com>
2021-07-20 23:35:10 +0900

    회원가입 예외처리 뷰 적용 toastity

--- src/Components/Signup/SignupContainer.js
+++ src/Components/Signup/SignupContainer.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
+import SignupPresenter from "./SignupPresenter";
+
+import { signup } from "../../Redux/async/user";
+import Footer from "../../shared/Footer";
 
 import { useDispatch } from "react-redux";
 
 import { Grid } from "@material-ui/core";
-import Footer from "../../shared/Footer";
-import SignupPresenter from "./SignupPresenter";
-import { signup } from "../../Redux/async/user";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from "react-toastify";
 
 const SignupContainer = (props) => {
     const dispatch = useDispatch();
@@ -17,6 +20,8 @@ const SignupContainer = (props) => {
     const [phoneNumber_1, setPhoneNumber_1] = useState("");
     const [phoneNumber_2, setPhoneNumber_2] = useState("");
     const [phoneNumber_3, setPhoneNumber_3] = useState("");
+
+    const notify = (text) => toast.error(text);
 
     function pad(n) {
         return n < 10 ? "0" + n : n;
@@ -70,7 +75,28 @@ const SignupContainer = (props) => {
             password_2,
             birthDate,
         };
-        dispatch(signup(data));
+        if (
+            name === "" ||
+            email === "" ||
+            phoneNumber === "" ||
+            password_1 === "" ||
+            password_2 === "" ||
+            birthDate === ""
+        ) {
+            notify("모든 정보를 입력해주세요....");
+        } else if (name.length < 2) {
+            notify("이름은 3자 이상 입력하세요.");
+        } else if (password_1 !== password_2) {
+            notify("비밀번호가 같지 않습니다.");
+        } else if (password_1.length < 8) {
+            notify("비밀번호는 8자 이상 입력하세요.");
+        } else if (phoneNumber.length < 13) {
+            notify("정확한 휴대폰 번호를 입력하세요.");
+        } else if (email.includes("@") === false) {
+            notify("이메일 형식을 확인하세요");
+        } else {
+            dispatch(signup(data));
+        }
     };
 
     return (
@@ -88,6 +114,18 @@ const SignupContainer = (props) => {
                 handlePhoneNumber={handlePhoneNumber}
                 handleSubmit={handleSubmit}
                 setDateValue={setDateValue}
+            />
+            <ToastContainer
+                limit={1}
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
             />
             <Grid style={{ marginLeft: "50px" }}>
                 <Footer />
