5abaa4d73987700297b0629c4afdb39d338d454d
choice91 <wer1264@gmail.com>
2021-07-22 23:26:59 +0900

    HeaderLink.js css수정
    요소간높이맞춤

--- src/shared/HeaderLinks.js
+++ src/shared/HeaderLinks.js
@@ -7,6 +7,7 @@ const useStyles = makeStyles({
     mainContainer: {
         display: "flex",
         justifyContent: "space-between",
+        alignItems: "center",
         color: (props) => (props.page === "main" ? "#fff" : "#000"),
     },
     divider: {
